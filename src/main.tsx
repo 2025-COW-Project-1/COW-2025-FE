e744d6e5686d087ae39037d5abcf6511aee15774
BXXNXXII <pgb0511@naver.com>
2026-02-08 21:14:41 +0900

    chore: React StrictMode 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
@@ -7,14 +6,11 @@ import ToastProvider from './components/toast/ToastProvider';
 import ConfirmProvider from './components/confirm/ConfirmProvider';
 
 createRoot(document.getElementById('root')!).render(
-  // StrictMode : 개발자용; 버그 더 빨리 드러냄
-  <StrictMode>
-    <ToastProvider>
-      <ConfirmProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ConfirmProvider>
-    </ToastProvider>
-  </StrictMode>
+  <ToastProvider>
+    <ConfirmProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ConfirmProvider>
+  </ToastProvider>
 );
