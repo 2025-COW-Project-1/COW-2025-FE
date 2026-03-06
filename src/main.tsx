9d6f9053be9758b379a1d94ec0be5d24e4b9f64b
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2026-03-06 20:13:07 +0900

    feat: QueryClient 설정 및 QueryClientProvider, ReactQueryDevtools 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,30 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './index.css';
 import App from './App';
 import ToastProvider from './components/toast/ToastProvider';
 import ConfirmProvider from './components/confirm/ConfirmProvider';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 3,
+      retry: 1,
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
-  <ToastProvider>
-    <ConfirmProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ConfirmProvider>
-  </ToastProvider>
+  <QueryClientProvider client={queryClient}>
+    <ToastProvider>
+      <ConfirmProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ConfirmProvider>
+    </ToastProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
